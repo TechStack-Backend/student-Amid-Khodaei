7e9574a5e5095f0470ea8a20dbd0ea0065c0bcc3
Amidkhodaei <amidkhodaie84@gmail.com>
2025-08-21 21:52:10 +0330

    Delete task1 directory

--- task1/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.Home),
-    path('Developers/', views.Developers),
-    path('Developers/CV/<int:cv_id>/', views.CV)
-]
